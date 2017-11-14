50a36381d07d6e2f3c2f5779026a8c9c50a00278
santiago garcia da rosa <sanganja@gmail.com>
2017-11-14 16:11:35 -0300

    changes to file names

--- vue-ssr/webpack.server.config.js
+++ vue-ssr/webpack.server.config.js
@@ -5,12 +5,12 @@ var baseWebpackConfig = require('./webpack.config')
 var webpackConfig = merge(baseWebpackConfig, {
     target: 'node',
     entry: {
-        app: './src/server.js'
+        app: './src/entry-server.js'
     },
     devtool: false,
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'server.js',
+        filename: 'entry-server.js',
         libraryTarget: 'commonjs2'
     },
     externals: Object.keys(require('./package.json').dependencies),
